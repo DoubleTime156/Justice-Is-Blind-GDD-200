aaa1a7a173da388d7c90f3178eeafb63caf8f43f
mrichter-mscs <111782117+mrichter-mscs@users.noreply.github.com>
2025-10-16 18:00:00 -0500

    Small fixes for level

--- Assets/Scripts/FOW/CaneTap.cs
+++ Assets/Scripts/FOW/CaneTap.cs
@@ -8,6 +8,10 @@ public class CaneTap : MonoBehaviour
     public PlayerPosition playerVision;
     public FogManager fogManager;
     public float noiseMultiplier = 2f;
+<<<<<<< Updated upstream
+=======
+    public float cooldown = 1f;
+>>>>>>> Stashed changes
     public float noiseDuration = 1f;
     public float cooldown = 2f;
 
