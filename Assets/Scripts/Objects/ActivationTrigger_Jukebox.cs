3a9c277ca887b1b2208f5474b0e00ce7d2b1b9ec
DoubleTime156 <ryankrum156@gmail.com>
2025-10-20 15:47:19 -0500

    Puts in the outdated ISH
    Still useful because the ISH has an interface class that can still be useful

--- Assets/Scripts/Objects/ActivationTrigger_Jukebox.cs
+++ Assets/Scripts/Objects/ActivationTrigger_Jukebox.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 
-public class ActivationTrigger : MonoBehaviour, BehaviorCallable
+public class ActivationTrigger_Jukebox : MonoBehaviour, BehaviorCallable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Behavior()
     {
         //this is where to put the behavior of the object when interacted with
+        Debug.Log("Hey! I, the JukeBox, have been activated.");
     }
 }
