f88a061a94ed8d485752ec08e774239e2a11fcb3
KirbywithaGun-dev <bwwohlfeil@gmail.com>
2025-11-04 16:40:58 -0600

    Main Menu
    Main menu added with working start button and quit button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,17 @@ public class GameManager : MonoBehaviour
         data.inventory[1] = 0;
         data.heldItem = 0;
     }
+
+    public void startGame() // Start game from beginning when Start is pressed on main menu
+    {
+        SceneManager.LoadScene("Level_Tutorial");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting Game");
+    }
     public void gameOver()
     {
         gameOverText.gameObject.SetActive(true);
