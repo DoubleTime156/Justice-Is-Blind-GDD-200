8904afb7119ac7277edcdde76dbcfb0091739b48
KirbywithaGun-dev <bwwohlfeil@gmail.com>
2025-11-27 14:23:27 -0600

    UI art assets
    Added button graphics for UI buttons. Also replaced the star particle with a custom asset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,6 @@ public class GameManager : MonoBehaviour
 {
     public PlayerData data;
 
-    public TextMeshProUGUI gameOverText;
-    public Button restartButton;
-    public Button quitButton;
-
-
     public void startGame() // Start game from beginning when Start is pressed on main menu
     {
         SceneManager.LoadScene("Level_Tutorial");
@@ -22,13 +17,6 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Exiting Game");
-    }
-    public void gameOver()
-    {
-        Debug.Log("Game Over");
-        gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(true);
-        quitButton.gameObject.SetActive(true);
     }
     public void restartGame() // Reload the scene when restart is clicked
     {
