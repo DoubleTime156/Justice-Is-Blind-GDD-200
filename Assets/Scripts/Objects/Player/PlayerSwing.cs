cc324888fd698209f50e99cdb7ccbd4128b773bf
K-D0G <kellylukasandrew@gmail.com>
2025-12-01 15:44:16 -0600

    Animation Final i sware #8 billion

--- Assets/Scripts/Objects/Player/PlayerSwing.cs
+++ Assets/Scripts/Objects/Player/PlayerSwing.cs
@@ -8,10 +8,15 @@ public class PlayerSwing : MonoBehaviour {
     public float playerViewAngle;
     public ParticleSystem knockoutParticles;
 
+    [SerializeField] private PersonAnimator personAnimator;
+    [SerializeField] private Animator _animator;
+
     public void OnSwing(InputAction.CallbackContext context)
     {
         // Only handle swing when the action is performed
         if (!context.performed) return;
+
+        _animator.SetTrigger("OnSwing");
         
         // Find all enemies inside the view radius
         Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(transform.position, playerViewRadius, LayerMask.GetMask("Enemy"));
