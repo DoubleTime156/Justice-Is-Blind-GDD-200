4a03bb558fd59c21c7a5835a57489ba5a2b12485
mrichter-mscs <111782117+mrichter-mscs@users.noreply.github.com>
2025-12-04 14:27:32 -0600

    More

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,23 +11,27 @@ public class GameManager : MonoBehaviour
     public void startGame() // Start game from beginning when Start is pressed on main menu
     {
         SceneManager.LoadScene("Level_Tutorial");
+        Time.timeScale = 1f;
     }
 
     public void quitGame()
     {
         Application.Quit();
         Debug.Log("Exiting Game");
+        Time.timeScale = 1f;
     }
     public void restartGame() // Reload the scene when restart is clicked
     {
         Debug.Log("restart");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
     }
 
     public void mainMenu() // Load main menu scene
     {
         Debug.Log("Quit to menu");
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
     }
    
 }
