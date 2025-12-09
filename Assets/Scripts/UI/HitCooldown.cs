2cf335deed14ac5a399ba5905f515e18feb94327
mrichter-mscs <111782117+mrichter-mscs@users.noreply.github.com>
2025-12-08 20:51:15 -0600

    Endscreen change

---
+++ Assets/Scripts/UI/HitCooldown.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HitCooldown : MonoBehaviour
+{
+
+    private float cooldown = 100f;
+    public float multipler = 10f;
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Updat
+    {
+        if 
+
+        scrollbar.value = 
+    }
+}
